e6fc46401d016c5d119737af0318116cf3970888
storycraft <storycraft@pancake.sh>
2021-11-19 13:50:54 +0900

    Removing old projects

--- graphics/src/data/mod.rs
+++
@@ -1,7 +0,0 @@
-/*
- * Created on Mon Sep 06 2021
- *
- * Copyright (c) storycraft. Licensed under the MIT Licence.
- */
-
-pub mod observable;
--- graphics/src/unit.rs
+++
@@ -1,8 +0,0 @@
-/*
- * Created on Sat Sep 11 2021
- *
- * Copyright (c) storycraft. Licensed under the MIT Licence.
- */
-
-pub struct PixelUnit;
-pub struct WgpuUnit;
