515af6aa7f5a4eb2add957344d987251ca7ce401
storycraft <storycraft@pancake.sh>
2022-06-27 01:39:02 +0900

    Fix test

--- components/text/tests/cache.rs
+++ components/text/tests/cache.rs
@@ -1,8 +1,8 @@
-use std::error::Error;
+use std::{error::Error, borrow::Cow};
 
 use rustybuzz::{Face, UnicodeBuffer};
 use storyboard_render::{backend::{BackendOptions, StoryboardBackend}, wgpu::{Backends, Instance}};
-use storyboard_text::{cache::GlyphCache};
+use storyboard_text::{cache::GlyphCache, font::Font};
 
 pub static FONT: &'static [u8] = include_bytes!("./NotoSansCJKkr-Regular.otf");
 
@@ -16,17 +16,17 @@ fn layout_test() -> Result<(), Box<dyn Error>> {
     ))
     .unwrap();
     
-    let face = Face::from_slice(FONT, 0).unwrap();
+    let font = Font::new(Cow::Borrowed(FONT), 0)?;
     
     let mut buffer = UnicodeBuffer::new();
     buffer.push_str("Hello world");
 
-    let buffer = rustybuzz::shape(&face, &[], buffer);
+    let buffer = rustybuzz::shape(&Face::from_slice(FONT, 0).unwrap(), &[], buffer);
     
     let mut cache = GlyphCache::new();
 
     let mut indices_iter = buffer.glyph_infos().iter().map(|info| info.glyph_id as u16).peekable();
-    while let Some(batch) = cache.batch(backend.device(), backend.queue(), &face, &mut indices_iter, 16) {
+    while let Some(batch) = cache.batch(backend.device(), backend.queue(), &font, &mut indices_iter, 16) {
         println!("batch: {:?}\n", batch);
     }
 
