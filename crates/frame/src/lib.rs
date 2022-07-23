1f9b57425b8cd7775c0210a6b4abc33cacfcdd9b
storycraft <storycraft@pancake.sh>
2022-07-23 20:11:02 +0900

    Add frame crates. Add stars example.

---
+++ crates/frame/src/lib.rs
@@ -0,0 +1,108 @@
+use downcast::{downcast, Any};
+use rustc_hash::{FxHashSet, FxHasher};
+use std::{marker::PhantomData, hash::BuildHasherDefault, fmt::Debug};
+use storyboard_render::task::RenderTask;
+
+use indexmap::IndexMap;
+
+#[derive(Debug)]
+pub struct FrameContainer {
+    state_map: IndexMap<usize, Box<dyn FrameComponent>, BuildHasherDefault<FxHasher>>,
+    dirty_list: FxHashSet<usize>,
+}
+
+impl FrameContainer {
+    pub fn new() -> Self {
+        Self {
+            state_map: IndexMap::default(),
+            dirty_list: FxHashSet::default(),
+        }
+    }
+
+    pub fn invalidated(&self) -> bool {
+        !self.dirty_list.is_empty()
+    }
+
+    pub fn values(&self) -> impl Iterator<Item = &Box<dyn FrameComponent>> {
+        self.state_map.values()
+    }
+
+    pub fn contains<T: FrameComponent>(&self, key: &FrameComponentKey<T>) -> bool {
+        self.state_map.contains_key(&key.0)
+    }
+
+    pub fn get<T: FrameComponent>(&self, key: &FrameComponentKey<T>) -> Option<&T> {
+        let boxed = self.state_map.get(&key.0)?;
+
+        boxed.downcast_ref().ok()
+    }
+
+    pub fn get_mut<T: FrameComponent>(&mut self, key: &FrameComponentKey<T>) -> Option<&mut T> {
+        let boxed = self.state_map.get_mut(&key.0)?;
+        self.dirty_list.insert(key.0);
+
+        boxed.downcast_mut().ok()
+    }
+
+    pub fn add_component<T: FrameComponent>(&mut self, state: T) -> FrameComponentKey<T> {
+        let boxed = Box::new(state);
+        let key = FrameComponentKey((&*boxed as *const _) as usize, PhantomData);
+
+        self.state_map.insert(key.0, boxed);
+        self.dirty_list.insert(key.0);
+
+        key
+    }
+
+    pub fn remove<T: FrameComponent>(&mut self, key: FrameComponentKey<T>) -> Option<T> {
+        let boxed = self.state_map.remove(&key.0)?.downcast::<T>().ok()?;
+        Some(*boxed)
+    }
+
+    pub fn update(&mut self) {
+        self.dirty_list.retain(|id| {
+            let mut flag = false;
+            let mut expired = false;
+
+            if let Some(state) = self.state_map.get_mut(id) {
+                flag = state.update();
+
+                expired = state.expired();
+            }
+
+            if expired {
+                self.state_map.remove(id);
+                return false;
+            }
+
+            flag
+        });
+    }
+}
+
+pub trait FrameComponent: Any {
+    fn expired(&self) -> bool;
+
+    fn update(&mut self) -> bool;
+
+    fn draw(&self, task: &mut RenderTask);
+}
+
+downcast!(dyn FrameComponent);
+
+impl Debug for dyn FrameComponent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("ScreenObject").finish_non_exhaustive()
+    }
+}
+
+#[derive(Debug)]
+pub struct FrameComponentKey<T>(usize, PhantomData<T>);
+
+impl<T> Clone for FrameComponentKey<T> {
+    fn clone(&self) -> Self {
+        Self(self.0, PhantomData)
+    }
+}
+
+impl<T> Copy for FrameComponentKey<T> {}
---
+++ examples/stars/src/main.rs
@@ -0,0 +1,243 @@
+#![feature(generic_associated_types)]
+
+use std::time::{Duration, Instant};
+
+use rand::Rng;
+use storyboard::{
+    app::{StoryboardApp, StoryboardAppProp, StoryboardAppState},
+    core::{
+        color::{Color, ShapeColor},
+        euclid::{rect, Angle, Rect, Transform3D},
+        unit::LogicalPixelUnit,
+    },
+    render::{
+        backend::BackendOptions,
+        task::RenderTask,
+        wgpu::{Limits, PowerPreference, PresentMode},
+    },
+    winit::{
+        event::Event,
+        event_loop::{EventLoop, ControlFlow},
+        window::{Window, WindowBuilder},
+    },
+    Storyboard,
+};
+use storyboard_box2d::{Box2D, Box2DStyle};
+use storyboard_frame::{FrameComponent, FrameContainer};
+use storyboard_state::{State, StateData, StateStatus, StateSystem, SystemStatus};
+
+fn main() {
+    use futures::executor::block_on;
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_title("Storyboard example player")
+        .build(&event_loop)
+        .unwrap();
+
+    block_on(main_async(event_loop, window));
+}
+
+async fn main_async(event_loop: EventLoop<()>, window: Window) {
+    let storyboard = Storyboard::init(
+        window,
+        &BackendOptions {
+            power_preference: PowerPreference::HighPerformance,
+            limits: Limits::downlevel_webgl2_defaults(),
+            ..Default::default()
+        },
+        PresentMode::AutoNoVsync,
+        None,
+    )
+    .await
+    .unwrap();
+
+    storyboard.run(
+        event_loop,
+        App::new(SimpleAnimApp::new(Duration::from_millis(50))),
+    );
+}
+
+#[derive(Debug)]
+pub struct App {
+    inital_state: Option<Box<dyn State<AppStateData>>>,
+    state: Option<StateSystem<AppStateData>>,
+    container: FrameContainer,
+}
+
+impl App {
+    pub fn new(inital_state: impl State<AppStateData> + 'static) -> Self {
+        Self {
+            inital_state: Some(Box::new(inital_state)),
+            state: None,
+            container: FrameContainer::new(),
+        }
+    }
+}
+
+impl StoryboardApp for App {
+    fn load(&mut self, prop: &StoryboardAppProp) {
+        if let Some(inital_state) = self.inital_state.take() {
+            self.state = Some(StateSystem::new(inital_state, prop));
+        }
+    }
+
+    fn unload(&mut self, _: &StoryboardAppProp) {}
+
+    fn update(&mut self, app_prop: &StoryboardAppProp, app_state: &mut StoryboardAppState) {
+        match app_state.event {
+            Event::RedrawRequested(_) => {
+                self.container.update();
+                for components in self.container.values() {
+                    components.draw(app_state.render_task);
+                }
+                app_state.render();
+            }
+
+            Event::MainEventsCleared => {
+                if let Some(state) = &mut self.state {
+                    *app_state.control_flow = match state.run(app_prop, &mut self.container) {
+                        SystemStatus::Poll => ControlFlow::Poll,
+                        SystemStatus::Wait => ControlFlow::Wait,
+                    }
+                }
+
+                if self.container.invalidated() {
+                    app_prop.window.request_redraw();
+                }
+            }
+
+            _ => {}
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct AppStateData;
+impl StateData for AppStateData {
+    type Prop<'p> = StoryboardAppProp;
+    type State<'s> = FrameContainer;
+}
+
+#[derive(Debug)]
+pub struct SimpleAnimApp {
+    elapsed: Duration,
+    interval: Duration,
+}
+
+impl SimpleAnimApp {
+    pub fn new(interval: Duration) -> Self {
+        Self {
+            elapsed: interval,
+            interval,
+        }
+    }
+}
+
+impl State<AppStateData> for SimpleAnimApp {
+    fn load<'p>(&mut self, _: &<AppStateData as StateData>::Prop<'p>) {}
+
+    fn unload<'p>(&mut self, _: &<AppStateData as StateData>::Prop<'p>) {}
+
+    fn update<'p, 's>(
+        &mut self,
+        system_prop: &<AppStateData as StateData>::Prop<'p>,
+        system_state: &mut <AppStateData as StateData>::State<'s>,
+    ) -> StateStatus<AppStateData> {
+        self.elapsed += system_prop.elapsed;
+
+        if self.elapsed >= self.interval {
+            self.elapsed = Duration::ZERO;
+
+            let mut rng = rand::thread_rng();
+
+            system_state.add_component(FadingStar::new(
+                Instant::now(),
+                Duration::from_millis(300 + (rng.gen::<f32>() * 2000.0) as u64),
+                rect(
+                    rng.gen::<f32>() * system_prop.window.inner_size().width as f32,
+                    rng.gen::<f32>() * system_prop.window.inner_size().height as f32,
+                    48.0,
+                    48.0,
+                ),
+                (1.0, 1.0, 1.0, 1.0).into(),
+            ));
+        }
+
+        StateStatus::Poll
+    }
+}
+
+#[derive(Debug)]
+pub struct FadingStar {
+    start: Instant,
+    duration: Duration,
+
+    bounds: Rect<f32, LogicalPixelUnit>,
+    color: Color,
+
+    alpha: f32,
+}
+
+impl FadingStar {
+    pub fn new(
+        start: Instant,
+        duration: Duration,
+        bounds: Rect<f32, LogicalPixelUnit>,
+        color: Color,
+    ) -> Self {
+        Self {
+            start,
+            duration,
+
+            bounds,
+            color,
+
+            alpha: 1.0,
+        }
+    }
+}
+
+impl FrameComponent for FadingStar {
+    fn expired(&self) -> bool {
+        self.start.elapsed() > self.duration
+    }
+
+    fn update(&mut self) -> bool {
+        self.alpha =
+            1.0 - self.start.elapsed().as_millis() as f32 / self.duration.as_millis() as f32;
+        true
+    }
+
+    fn draw(&self, task: &mut RenderTask) {
+        task.push(Box2D {
+            bounds: self.bounds,
+            texture: None,
+            fill_color: ShapeColor::Single(
+                (
+                    self.color.red,
+                    self.color.green,
+                    self.color.blue,
+                    self.color.alpha * self.alpha,
+                )
+                    .into(),
+            ),
+            border_color: ShapeColor::TRANSPARENT,
+            style: Box2DStyle::default(),
+            transform: Transform3D::translation(
+                -self.bounds.origin.x - self.bounds.size.width / 2.0,
+                -self.bounds.origin.y - self.bounds.size.height / 2.0,
+                0.0,
+            )
+            .then_rotate(0.0, 0.0, 1.0, Angle::degrees(self.alpha * 360.0))
+            .then_translate(
+                (
+                    self.bounds.origin.x + self.bounds.size.width / 2.0,
+                    self.bounds.origin.y + self.bounds.size.height / 2.0,
+                    0.0,
+                )
+                    .into(),
+            ),
+        })
+    }
+}
